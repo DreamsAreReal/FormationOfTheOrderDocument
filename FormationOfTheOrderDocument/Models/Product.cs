9a27640a0e1f7abf4543b8c44e915d2d63fd6294
Aleaxnder <mail@mail.com>
2020-11-28 18:55:20 +0300

    Done models and excelclient

---
+++ FormationOfTheOrderDocument/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationOfTheOrderDocument.Models
+{
+    class Product
+    {
+        public string BarCode { get; private set; }
+
+        public string Count { get; private set; }
+
+        public string Price { get; private set; }
+
+        public string Name { get; private set; }
+
+        public Product(string barcode, string count, string price, string name)
+        {
+            BarCode = barcode;
+            Count = count;
+            Price = price;
+            Name = name;
+        }
+    }
+}
