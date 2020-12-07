10a7fe2ca9b40c9de3994032807f01bd8478fd0f
Alexander <46569007+ProgWidness@users.noreply.github.com>
2020-12-07 13:45:26 +0300

    fix program

--- FormationOfTheOrderDocument/Program.cs
+++ FormationOfTheOrderDocument/Program.cs
@@ -12,7 +12,7 @@ namespace FormationOfTheOrderDocument
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
-        static void Main(112312312412421)
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
